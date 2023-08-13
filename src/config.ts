7c7df951d501b9ae345a35d1e0cda34d903ff3e7
shigatena <50152760+shigatena666@users.noreply.github.com>
2023-08-13 17:35:29 +0200

    Updated website, blog moved to https://github.com/shigatena666/wiki.shigatena.dev

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const TITLE_TEMPLATE = 'shigatena.dev - ';
-export const SITE_DESCRIPTION = 'Blog, Resume, Projects and more of shigatena';
+export const SITE_DESCRIPTION = 'Resume, Projects and more of shigatena';
 export const BUY_ME_COFFEE = "https://bmc.link/shigatena666"
 export const DEFAULT_THEME = "dracula"
