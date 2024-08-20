ad01ff14f4966363346abce1bb660cdfa47556a3
shigatena666 <github@shigatena.dev>
2024-08-21 01:02:49 +0200

    updated astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import db from "@astrojs/db";
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
-import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
 
 // https://astro.build/config
@@ -25,7 +24,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    db(),
     simpleStackForm(),
   ],
   output: "hybrid",
